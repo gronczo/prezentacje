68b55d9c7452bd6dbfa8e8bd0bfc59a10c243eb0
unknown <Adam@.(none)>
2012-03-25 22:54:46 +0200

    uklad strony

--- presentations/views.py
+++ presentations/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import *
 # Create your views here.
 
 def index(request):
-    return render_to_response('index.html')
+    return render_to_response('index.html', context_instance=RequestContext(request))
+
+def login(request):
+    return render_to_response('login.html', context_instance=RequestContext(request))
